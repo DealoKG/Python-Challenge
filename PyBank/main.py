1566717f26a9470e4f2b4a5b86e911aec0a792f3
DealoKG <152652308+DealoKG@users.noreply.github.com>
2024-02-11 23:53:45 -0500

    updates to the python challenge assignment

---
+++ PyBank/main.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+# Define the path of the csv file
+budget_csv = os.path.join("/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyBank/Resources/budget_data.csv")
+
+#Initialize the variables
+
+tot_months = 0
+tot_prof_loss = 0
+ini_change_prof = 0
+change_prof_loss = []
+greatest_inc = ["", 0]
+greatest_dec = ["", 0]
+
+#Open the csv file
+with open(budget_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
+    
+    #read the header riw first
+    csv_header = next(csvreader)
+
+     # Iterate over the csv reader object
+    for budget in csvreader:
+        tot_months += 1
+        tot_prof_loss += int(budget[1])
+        
+        change = int(budget[1]) - ini_change_prof
+        
+        ini_change_prof = int(budget[1])
+        change_prof_loss.append(change)
+        
+     # Calculate the greatest increase in profits
+        if change > greatest_inc[1]:
+            greatest_inc[0] = budget[0]
+            greatest_inc[1] = change
+
+        # Calculate the greatest decrease in profits
+        if change < greatest_dec[1]:
+            greatest_dec[0] = budget[0]
+            greatest_dec[1] = change
+
+  
+# Calculate the average change in profit/losses
+average_change = sum(change_prof_loss[1:]) / (tot_months - 1)
+
+
+# Print the financial analysis to the terminal
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {tot_months}")
+print(f"Total: ${tot_prof_loss}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_inc[0]} (${greatest_inc[1]})")
+print(f"Greatest Decrease in Profits: {greatest_dec[0]} (${greatest_dec[1]})")
+
+
+
+# Write results to CSV file
+with open("/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyBank/analysis/budget_analysis.csv", "w", newline="") as csvfile:
+
+    csvwriter = csv.writer(csvfile, delimiter=",")
+    
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow([f"Total Months: {tot_months}"])
+    csvwriter.writerow([f"Total: ${tot_prof_loss}"])
+    csvwriter.writerow([f"Average Change: ${average_change:.2f}"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {greatest_inc[0]} (${greatest_inc[1]})"])
+    csvwriter.writerow([f"Greatest Decrease in Profits: {greatest_dec[0]} (${greatest_dec[1]})"])
+
+print(f"Results exported to the local file located in '/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyBank/analysis/budget_analysis.csv'.")
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,65 @@
+import os
+import csv
+
+# Define the path of the csv file
+election_csv = os.path.join("/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyPoll/Resources/election_data.csv")
+
+#Initialize the variables
+
+vote_result = 0
+can_votes = {}
+winner = ""
+winner_votes = 0
+
+#Open the csv file
+with open(election_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
+    
+    #read the header riw first
+    csv_header = next(csvreader)
+    
+
+
+    # Iterate over the csv reader object
+    for data in csvreader:
+        vote_result += 1
+        
+        can_name = data[2]
+        
+        if can_name not in can_votes:
+            can_votes[can_name] = 0
+        can_votes[can_name] += 1
+
+# Print the election results to the terminal
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {vote_result}")
+print("-------------------------")
+for candidate, votes in can_votes.items():
+    percentage = (votes / vote_result) * 100
+    print(f"{candidate}: {percentage:.3f}% ({votes})")
+    if votes > winner_votes:
+        winner_votes = votes
+        winner = candidate
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+
+# Write results to CSV file
+with open("/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyPoll/analysis/election_analysis.csv", "w", newline="") as csvfile:
+
+    csvwriter = csv.writer(csvfile, delimiter=",")
+    
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow([f"Total Votes: {vote_result}"])
+    for candidate, votes in can_votes.items():
+        percentage = (votes / vote_result) * 100
+        csvwriter.writerow([f"{candidate}: {percentage:.3f}% ({votes})"])
+    csvwriter.writerow([f"Winner: {winner}"])
+
+    
+    
+print(f"Results exported to the local file located in  '/Users/lordfranko/Desktop/PythonChallenge3/Python-Challenge/PyPoll/analysis/election_analysis.csv'.")
+
+
